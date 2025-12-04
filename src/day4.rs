82d324e1155c1f4683c0d24a6f83197eaa58a67d
Ben Wiklund <ben@daisyowl.com>
2025-12-03 23:15:03 -0800

    cleanup

--- src/day4.rs
+++ src/day4.rs
@@ -1,52 +1,78 @@
-pub fn solve(part: u32) -> u32 {
-    let grid: Vec<_> = std::fs::read_to_string("./src/day4_input.txt")
-        .unwrap()
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|ch| match ch {
-                    '@' => true,
-                    '.' => false,
-                    _ => panic!(),
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect();
+struct Grid {
+    cells: Vec<bool>,
+    w: usize,
+    h: usize,
+}
 
-    fn is_occupied(grid: &Vec<Vec<bool>>, x: i32, y: i32) -> bool {
-        let h = grid.len() as i32;
-        let w = grid[0].len() as i32; // don't care about empty case
-        if y < 0 || x < 0 || y >= h || x >= w {
+impl Grid {
+    pub fn from_vv(vv: Vec<Vec<bool>>) -> Option<Self> {
+        // a real program would check for errors in row size but i am lazy
+        Some(Grid {
+            cells: vv.iter().flatten().copied().collect(),
+            w: vv.len(),
+            h: vv[0].len(),
+        })
+    }
+
+    // convenience fn that returns false safely for oob entries
+    fn is_occupied(&self, x: i32, y: i32) -> bool {
+        if y < 0 || x < 0 || y >= self.h as i32 || x >= self.w as i32 {
             return false;
         }
 
-        grid[y as usize][x as usize]
+        self.cells[y as usize * self.w + x as usize]
     }
 
-    fn neighbor_count(grid: &Vec<Vec<bool>>, x: i32, y: i32) -> u32 {
+    fn neighbor_count(&self, x: i32, y: i32) -> u32 {
         let mut count = 0;
         for iy in y - 1..=y + 1 {
             for ix in x - 1..=x + 1 {
                 if ix == x && iy == y {
                     continue;
                 }
-                if is_occupied(grid, ix, iy) {
+                if self.is_occupied(ix, iy) {
                     count += 1;
                 }
             }
         }
         count
     }
+}
 
-    let h = grid.len() as i32;
-    let w = grid[0].len() as i32; // don't care about empty case
-    (0..h)
-        .map(|iy| {
-            (0..w)
-                .filter(|&ix| is_occupied(&grid, ix, iy) && neighbor_count(&grid, ix, iy) < 4)
-                .count() as u32
-        })
-        .sum::<u32>()
+pub fn solve(part: u32) -> u32 {
+    let grid = Grid::from_vv(
+        std::fs::read_to_string("./src/day4_input.txt")
+            .unwrap()
+            .lines()
+            .map(|l| {
+                l.chars()
+                    .map(|ch| match ch {
+                        '@' => true,
+                        '.' => false,
+                        _ => panic!(),
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect(),
+    )
+    .unwrap();
+
+    match part {
+        0 => (0..grid.h)
+            .map(|iy| {
+                (0..grid.w)
+                    .filter(|&ix| {
+                        grid.is_occupied(ix as i32, iy as i32)
+                            && grid.neighbor_count(ix as i32, iy as i32) < 4
+                    })
+                    .count() as u32
+            })
+            .sum::<u32>(),
+
+        1 => panic!("idk yet"),
+
+        _ => panic!("there are no other parts"),
+    }
 }
 
 #[cfg(test)]
@@ -55,6 +81,6 @@ mod tests {
 
     #[test]
     fn day4() {
-        assert_eq!(solve(0), 9999999);
+        assert_eq!(solve(0), 1553);
     }
 }
