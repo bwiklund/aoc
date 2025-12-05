de41e15a4b9b60de46d2b34f01858edd2307fe51
Ben Wiklund <ben@daisyowl.com>
2025-12-04 23:38:47 -0800

    d5p2

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,10 @@
-pub fn solve(_part: u32) -> usize {
+#[derive(Debug)]
+enum IdRangeEntry {
+    Left(u64),
+    Right(u64),
+}
+
+pub fn solve(part: u32) -> u64 {
     let mut ranges = vec![];
     let mut ids = vec![];
     let mut is_ids_section = false;
@@ -13,14 +19,69 @@ pub fn solve(_part: u32) -> usize {
                 ids.push(line.parse::<u64>().unwrap())
             } else {
                 let (l, r) = line.split_once("-").unwrap();
-                ranges.push((l.parse::<u64>().unwrap(), r.parse::<u64>().unwrap()));
+                ranges.push((l.parse::<u64>().unwrap(), r.parse::<u64>().unwrap() + 1));
             }
         });
 
-    // brute force approach
-    ids.iter()
-        .filter(|id| ranges.iter().any(|(low, high)| *id >= low && *id <= high))
-        .count()
+    match part {
+        // first pass, brute force check the ranges
+        0 => ids
+            .iter()
+            .filter(|id| ranges.iter().any(|(low, high)| *id >= low && *id < high))
+            .count() as u64,
+
+        // how many fresh ids are there according to ranges (not caring about ids anymore)
+        // this will need to be clever because the ranges are like 15 digit numbers and we can't just scan
+        1 => {
+            // plan: make a sorted list of range start and ends, because they overlap.
+            // then scan through that list with an accumulator for range overlap amount, and sum up the size of the ranges when we're inside a range.
+            let mut ordered: Vec<IdRangeEntry> = ranges
+                .iter()
+                .flat_map(|(l, r)| vec![IdRangeEntry::Left(*l), IdRangeEntry::Right(*r)])
+                .collect();
+
+            ordered.sort_by_key(|r| match r {
+                IdRangeEntry::Left(n) => *n,
+                IdRangeEntry::Right(n) => *n,
+            });
+
+            // dbg!(&ordered);
+
+            let mut depth = 0;
+            let mut count = 0u64;
+            let mut current_id = 0u64;
+
+            for r in ordered {
+                let next_id;
+                match r {
+                    IdRangeEntry::Left(n) => {
+                        next_id = n;
+
+                        if depth > 0 {
+                            count += next_id - current_id;
+                        }
+
+                        depth += 1;
+                    }
+                    IdRangeEntry::Right(n) => {
+                        next_id = n;
+
+                        if depth > 0 {
+                            count += next_id - current_id;
+                        }
+
+                        depth -= 1;
+                    }
+                }
+
+                current_id = next_id;
+            }
+
+            count
+        }
+
+        _ => panic!(),
+    }
 }
 
 #[cfg(test)]
@@ -30,5 +91,6 @@ mod tests {
     #[test]
     fn day5() {
         assert_eq!(solve(0), 698);
+        assert_eq!(solve(1), 352807801032167);
     }
 }
