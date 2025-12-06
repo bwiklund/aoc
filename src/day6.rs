ef6f44315d8508be882857ab71e9e18d87312d1b
Ben Wiklund <ben@daisyowl.com>
2025-12-05 22:34:57 -0800

    p1 refactor to make p2 easier

--- src/day6.rs
+++ src/day6.rs
@@ -13,17 +13,34 @@ impl Op {
     }
 }
 
-pub fn solve(_part: u32) -> u64 {
+pub fn solve(part: u32) -> u64 {
     let txt = std::fs::read_to_string("./src/day6_input.txt").unwrap();
     let lines = txt.lines().collect::<Vec<_>>();
-    let numbers: Vec<_> = lines[0..lines.len() - 1]
-        .iter()
-        .map(|l| {
-            l.split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect::<Vec<u64>>()
-        })
-        .collect();
+    let columns: Vec<Vec<u64>> = match part {
+        0 => {
+            let rows = lines[0..lines.len() - 1]
+                .iter()
+                .map(|l| {
+                    l.split_whitespace()
+                        .map(|s| s.parse().unwrap())
+                        .collect::<Vec<u64>>()
+                })
+                .collect::<Vec<_>>();
+
+            // transpose
+            (0..rows[0].len())
+                .map(|col_idx| {
+                    (0..rows.len())
+                        .map(|row_idx| rows[row_idx][col_idx])
+                        .collect()
+                })
+                .collect()
+        }
+
+        1 => todo!("ASDF"),
+
+        _ => panic!(),
+    };
 
     let ops = lines[lines.len() - 1]
         .split_whitespace()
@@ -31,7 +48,7 @@ pub fn solve(_part: u32) -> u64 {
 
     ops.enumerate()
         .map(|(idx, op)| {
-            let operands = numbers.iter().map(|row| row[idx]);
+            let operands = columns[idx].iter();
             match op {
                 Op::Add => operands.sum::<u64>(),
                 Op::Multiply => operands.product(),
