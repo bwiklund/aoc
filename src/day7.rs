4b4e54b515d05b77132c31e081b80ff620064824
Ben Wiklund <ben@daisyowl.com>
2025-12-06 23:42:39 -0800

    this is too slow

--- src/day7.rs
+++ src/day7.rs
@@ -6,7 +6,7 @@ enum Tile {
     Splitter,
 }
 
-pub fn solve(_part: u32) -> u64 {
+pub fn solve(part: u32) -> u64 {
     let env: Vec<Vec<Tile>> = std::fs::read_to_string("./src/day7_input.txt")
         .unwrap()
         .lines()
@@ -22,28 +22,56 @@ pub fn solve(_part: u32) -> u64 {
         })
         .collect();
 
-    let mut beam = HashSet::new();
-    let mut split_count = 0;
-    for row in env {
-        for (idx, t) in row.iter().enumerate() {
-            match t {
-                Tile::None => {}
-                Tile::Origin => {
-                    beam.insert(idx);
-                }
-                Tile::Splitter => {
-                    if beam.contains(&idx) {
-                        beam.insert(idx - 1);
-                        beam.remove(&idx);
-                        beam.insert(idx + 1);
-                        split_count += 1;
+    if part == 0 {
+        let mut beam = HashSet::new();
+        let mut split_count = 0;
+        for row in env {
+            for (idx, t) in row.iter().enumerate() {
+                match t {
+                    Tile::None => {}
+                    Tile::Origin => {
+                        beam.insert(idx);
+                    }
+                    Tile::Splitter => {
+                        if beam.contains(&idx) {
+                            beam.insert(idx - 1);
+                            beam.remove(&idx);
+                            beam.insert(idx + 1);
+                            split_count += 1;
+                        }
                     }
                 }
             }
         }
+        return split_count;
     }
 
-    split_count
+    // // this might be totally inadquate to the numbers involved but lets see if this can work... just traverse and count...?
+    if part == 1 {
+        let x = env[0]
+            .iter()
+            .enumerate()
+            .find(|(_, t)| matches!(t, Tile::Origin))
+            .unwrap()
+            .0;
+
+        return rec_tachyon(&env, 1, x);
+
+        fn rec_tachyon(env: &Vec<Vec<Tile>>, depth: usize, x: usize) -> u64 {
+            if depth >= env.len() {
+                return 0;
+            }
+
+            match env[depth][x] {
+                Tile::Splitter => {
+                    rec_tachyon(env, depth + 1, x - 1) + rec_tachyon(env, depth + 1, x + 1)
+                }
+                _ => rec_tachyon(env, depth + 1, x),
+            }
+        }
+    }
+
+    panic!();
 }
 
 #[cfg(test)]
@@ -53,6 +81,6 @@ mod tests {
     #[test]
     fn day7() {
         assert_eq!(solve(0), 1499);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 0);
     }
 }
