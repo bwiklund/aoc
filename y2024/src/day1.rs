135996d2f017fa9ed7bd91b8fd1ff5fc6c26b523
Ben Wiklund <ben@daisyowl.com>
2025-12-14 22:19:08 -0800

    cleanup

--- y2024/src/day1.rs
+++ y2024/src/day1.rs
@@ -1,33 +1,32 @@
 use std::collections::HashMap;
 
 pub fn solve(part: u32) -> i64 {
-    let mut l1 = vec![];
-    let mut l2 = vec![];
-    std::fs::read_to_string("./src/day1_input.txt")
+    let (mut l1, mut l2): (Vec<i64>, Vec<i64>) = std::fs::read_to_string("./src/day1_input.txt")
         .unwrap()
         .lines()
-        .for_each(|l| {
-            if let [a, b] = l.split_ascii_whitespace().collect::<Vec<_>>()[..] {
-                l1.push(a.parse::<i64>().unwrap());
-                l2.push(b.parse::<i64>().unwrap());
-            }
-        });
+        .filter_map(|l| {
+            let mut parts = l.split_ascii_whitespace();
+            let a: i64 = parts.next()?.parse().ok()?;
+            let b: i64 = parts.next()?.parse().ok()?;
+            Some((a, b))
+        })
+        .unzip();
 
-    l1.sort();
-    l2.sort();
+    l1.sort_unstable();
+    l2.sort_unstable();
 
     match part {
         0 => l1.iter().zip(l2).map(|(a, b)| (a - b).abs()).sum(),
 
         1 => {
-            let mut l2counts = HashMap::<i64, i64>::new();
-            l2.iter().for_each(|n| {
-                *l2counts.entry(*n).or_insert(0) += 1;
+            let l2counts = l2.iter().fold(HashMap::<i64, i64>::new(), |mut acc, &n| {
+                *acc.entry(n).or_insert(0) += 1;
+                acc
             });
             l1.iter().map(|n| n * l2counts.get(n).unwrap_or(&0)).sum()
         }
 
-        _ => panic!(),
+        _ => unreachable!(),
     }
 }
 
