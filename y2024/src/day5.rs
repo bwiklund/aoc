205dc8ff2b2b7509a13857a5d13d4c3c3f058594
Ben Wiklund <ben@daisyowl.com>
2025-12-16 02:32:16 -0800

    d5p2

--- y2024/src/day5.rs
+++ y2024/src/day5.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn solve(part: u32) -> i64 {
     let mut rules = vec![];
     let mut updates = vec![];
@@ -23,29 +25,66 @@ pub fn solve(part: u32) -> i64 {
             }
         });
 
+    fn is_in_order(update: &Vec<i64>, rules: &Vec<(i64, i64)>) -> bool {
+        rules
+            .iter()
+            .filter(|r| update.contains(&r.0) && update.contains(&r.1))
+            .all(|r| update.iter().position(|&x| x == r.0) < update.iter().position(|&x| x == r.1))
+    }
+
+    fn fix(update: &Vec<i64>, rules: &Vec<(i64, i64)>) -> Vec<i64> {
+        // reduce to just the ones we care about
+        let rules = rules
+            .iter()
+            .filter(|r| update.contains(&r.0) && update.contains(&r.1))
+            .map(|r| *r)
+            .collect::<Vec<_>>();
+
+        let mut output = vec![];
+        let mut remaining = update.iter().collect::<HashSet<_>>();
+
+        // for each number in the input, do this:
+        // if there are no rules saying it has to be after a number remaining in the input, append it to the output.
+
+        // lots of copying here huh
+        while remaining.len() > 0 {
+            let mut found_p: Option<i64> = None;
+            for &&p in remaining.iter() {
+                if !rules
+                    .iter()
+                    .filter(|r| r.1 == p)
+                    .any(|r| remaining.contains(&r.0))
+                {
+                    found_p = Some(p);
+                }
+            }
+            if let Some(p) = found_p {
+                output.push(p);
+                remaining.remove(&p);
+            } else {
+                panic!("This shouldn't be possible");
+            }
+        }
+
+        output
+    }
+
     match part {
         0 => {
             // brute force first
             updates
                 .iter()
-                .map(|update| {
-                    if rules
-                        .iter()
-                        .filter(|r| update.contains(&r.0) && update.contains(&r.1))
-                        .all(|r| {
-                            update.iter().position(|&x| x == r.0)
-                                < update.iter().position(|&x| x == r.1)
-                        })
-                    {
-                        update[update.len() / 2]
-                    } else {
-                        0
-                    }
-                })
+                .filter(|update| is_in_order(update, &rules))
+                .map(|update| update[update.len() / 2])
                 .sum::<i64>()
         }
 
-        1 => 0,
+        1 => updates
+            .iter()
+            .filter(|update| !is_in_order(update, &rules))
+            .map(|update| fix(update, &rules))
+            .map(|update| update[update.len() / 2])
+            .sum::<i64>(),
 
         _ => unreachable!(),
     }
@@ -58,6 +97,6 @@ mod tests {
     #[test]
     fn day5() {
         assert_eq!(solve(0), 5087);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 4971);
     }
 }
