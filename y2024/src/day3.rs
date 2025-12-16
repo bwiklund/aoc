b71c6d95883bd79ab7a6b44752145667008d0af9
Ben Wiklund <ben@daisyowl.com>
2025-12-15 23:58:43 -0800

    d3p2

--- y2024/src/day3.rs
+++ y2024/src/day3.rs
@@ -21,6 +21,16 @@ impl Ctx {
         }
     }
 
+    fn accept_str<'a>(&mut self, ex: &'a str) -> Option<&'a str> {
+        let back = self.idx;
+        if ex.chars().all(|ch| self.accept(ch).is_some()) {
+            Some(ex)
+        } else {
+            self.idx = back;
+            None
+        }
+    }
+
     fn accept_range(&mut self, range: RangeInclusive<char>) -> Option<char> {
         if let Some(ch) = self.chars.get(self.idx)
             && range.contains(ch)
@@ -49,6 +59,36 @@ impl Ctx {
     }
 }
 
+fn parse(ctx: &mut Ctx, consider_dos: bool) -> i64 {
+    let mut accum = 0;
+    let mut enabled = true;
+    while !ctx.is_done() {
+        // any failure leaves idx unmodified and starts checking from the top with the last failed index. not good enough for a real language but good enough for the problem as stated so far...
+
+        let old_idx = ctx.idx;
+
+        if consider_dos && ctx.accept_str("do()").is_some() {
+            enabled = true;
+        } else if consider_dos && ctx.accept_str("don't()").is_some() {
+            enabled = false;
+        } else if ctx.accept_str("mul(").is_some()
+            && let Some(n1) = ctx.accept_number()
+            && ctx.accept(',').is_some()
+            && let Some(n2) = ctx.accept_number()
+            && ctx.accept(')').is_some()
+        {
+            if enabled {
+                accum += n1 * n2
+            }
+        } else {
+            if ctx.idx == old_idx {
+                ctx.idx += 1;
+            }
+        }
+    }
+    accum
+}
+
 pub fn solve(part: u32) -> i64 {
     let mut ctx = Ctx {
         chars: std::fs::read_to_string("./src/day3_input.txt")
@@ -59,29 +99,9 @@ pub fn solve(part: u32) -> i64 {
     };
 
     match part {
-        0 => {
-            let mut accum = 0;
-            while !ctx.is_done() {
-                // any failure leaves idx unmodified and starts checking from the top with the last failed index. not good enough for a real language but good enough for the problem as stated so far...
-                if ctx.accept('m').is_some() {
-                    if ctx.accept('u').is_some()
-                        && ctx.accept('l').is_some()
-                        && ctx.accept('(').is_some()
-                        && let Some(n1) = ctx.accept_number()
-                        && ctx.accept(',').is_some()
-                        && let Some(n2) = ctx.accept_number()
-                        && ctx.accept(')').is_some()
-                    {
-                        accum += n1 * n2
-                    }
-                } else {
-                    ctx.idx += 1;
-                }
-            }
-            accum
-        }
+        0 => parse(&mut ctx, false),
 
-        1 => 0,
+        1 => parse(&mut ctx, true),
 
         _ => unreachable!(),
     }
@@ -94,6 +114,6 @@ mod tests {
     #[test]
     fn day9() {
         assert_eq!(solve(0), 170778545);
-        // assert_eq!(solve(1), 0);
+        assert_eq!(solve(1), 82868252);
     }
 }
