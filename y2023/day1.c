d44fe3569e4f4907a170c46222a3cef0ecbcb293
Ben Wiklund <ben@daisyowl.com>
2025-12-17 18:40:59 -0800

    d1p2 answer with bad code

--- y2023/day1.c
+++ y2023/day1.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define DONE -1
 #define NEWLINE -2
@@ -8,16 +9,54 @@ void assert_eq(int a, int b) {
     printf("%d %d %s\n", a, b, a == b ? "pass" : "fail");
 }
 
+bool buff_ends_with(char *buff, int *b_idx, const char *str, int len) {
+    if (*b_idx >= len && (strncmp(buff + *b_idx - len, str, len) == 0)) {
+        // *b_idx = 0;
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int next_int(FILE *f) {
     // drains a file char by char looking for (positive) ints. returns -1 when
     // done. returns -2 on newline
     char ch;
+
+    static char buff[100000];
+    static int b_idx = 0;
+
     while ((ch = fgetc(f)) != EOF) {
-        if (ch == '\n') {
+        if (ch >= 'a' && ch <= 'z') {
+            buff[b_idx++] = ch;
+            if (buff_ends_with(buff, &b_idx, "zero", 4))
+                return 0;
+            if (buff_ends_with(buff, &b_idx, "one", 3))
+                return 1;
+            if (buff_ends_with(buff, &b_idx, "two", 3))
+                return 2;
+            if (buff_ends_with(buff, &b_idx, "three", 5))
+                return 3;
+            if (buff_ends_with(buff, &b_idx, "four", 4))
+                return 4;
+            if (buff_ends_with(buff, &b_idx, "five", 4))
+                return 5;
+            if (buff_ends_with(buff, &b_idx, "six", 3))
+                return 6;
+            if (buff_ends_with(buff, &b_idx, "seven", 5))
+                return 7;
+            if (buff_ends_with(buff, &b_idx, "eight", 5))
+                return 8;
+            if (buff_ends_with(buff, &b_idx, "nine", 4))
+                return 9;
+        } else if (ch == '\n') {
+            // b_idx = 0;
             return NEWLINE;
-        }
-        if (ch >= 48 && ch <= 57) { // is digit
-            return ch - 48;
+        } else if (ch >= '0' && ch <= '9') {
+            // b_idx = 0;
+            return ch - '0';
+        } else {
+            // b_idx = 0; // unreachable but lets reset just in case
         }
     }
     return DONE;
@@ -35,12 +74,14 @@ int main() {
 
     while ((n = next_int(f)) != DONE) {
         if (n == NEWLINE) {
+            printf("\n %d %d\n", first_num, last_num);
             int calibration_number = first_num * 10 + last_num;
             sum += calibration_number;
 
             first_num = -1;
             last_num = -1;
         } else {
+            printf("%d ", n);
             if (first_num == -1) {
                 first_num = n;
             }
