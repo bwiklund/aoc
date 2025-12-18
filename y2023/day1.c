d817ff7d2bb0ee5a97251a37950bf2c55d813257
Ben Wiklund <ben@daisyowl.com>
2025-12-17 19:22:02 -0800

    more

--- y2023/day1.c
+++ y2023/day1.c
@@ -6,20 +6,25 @@
 #define DONE -1
 #define NEWLINE -2
 
+// a dynamic array implementation
 typedef struct {
-    FILE *f;
     char *buff;
     int buff_size;
     int b_idx;
+} Buff;
+
+typedef struct {
+    FILE *f;
+    Buff buff;
 } Ctx;
 
 void assert_eq(int a, int b) {
     printf("%d %d %s\n", a, b, a == b ? "pass" : "fail");
 }
 
-bool buff_ends_with(Ctx *ctx, const char *str, int len) {
-    if (ctx->b_idx >= len &&
-        (strncmp(ctx->buff + ctx->b_idx - len, str, len) == 0)) {
+bool b_ends_with(Buff *buff, const char *str, int len) {
+    if (buff->b_idx >= len &&
+        (strncmp(buff->buff + buff->b_idx - len, str, len) == 0)) {
         return true;
     } else {
         return false;
@@ -27,59 +32,65 @@ bool buff_ends_with(Ctx *ctx, const char *str, int len) {
 }
 
 // i could just set a max line height tbh. but i wanted to write this by hand
-void ensure_size(Ctx *ctx) {
-    if (ctx->b_idx >= ctx->buff_size) {
+void b_ensure_size(Buff *buff) {
+    if (buff->b_idx >= buff->buff_size) {
         // starts 0, default 4, 1.5x growth rate
-        int new_size = ctx->buff_size == 0 ? 4 : ctx->buff_size * 1.5;
+        int new_size = buff->buff_size == 0 ? 4 : buff->buff_size * 1.5;
         char *doubled = malloc(new_size * sizeof(int));
-        memccpy(doubled, ctx->buff, ctx->buff_size, sizeof(int));
-        free(ctx->buff);
-        ctx->buff = doubled;
-        ctx->buff_size = new_size;
+        if (buff->buff != NULL) {
+            memccpy(doubled, buff->buff, buff->buff_size, sizeof(int));
+            free(buff->buff);
+        }
+        buff->buff = doubled;
+        buff->buff_size = new_size;
     }
 }
 
-void buffer(Ctx *ctx, char ch) {
-    ensure_size(ctx);
-    ctx->buff[ctx->b_idx++] = ch;
+void b_write(Buff *buff, char ch) {
+    b_ensure_size(buff);
+    buff->buff[buff->b_idx++] = ch;
 }
+
+void b_clear(Buff *buff) { buff->b_idx = 0; }
 
 int next_int(Ctx *ctx, bool p2) {
     // drains a file char by char looking for (positive) ints. returns -1 when
     // done. returns -2 on newline
     char ch;
 
+    Buff *buff = &ctx->buff;
+
     while ((ch = fgetc(ctx->f)) != EOF) {
         if (p2 && ch >= 'a' && ch <= 'z') {
-            buffer(ctx, ch);
-            if (buff_ends_with(ctx, "zero", 4))
+            b_write(buff, ch);
+            if (b_ends_with(buff, "zero", 4))
                 return 0;
-            if (buff_ends_with(ctx, "one", 3))
+            if (b_ends_with(buff, "one", 3))
                 return 1;
-            if (buff_ends_with(ctx, "two", 3))
+            if (b_ends_with(buff, "two", 3))
                 return 2;
-            if (buff_ends_with(ctx, "three", 5))
+            if (b_ends_with(buff, "three", 5))
                 return 3;
-            if (buff_ends_with(ctx, "four", 4))
+            if (b_ends_with(buff, "four", 4))
                 return 4;
-            if (buff_ends_with(ctx, "five", 4))
+            if (b_ends_with(buff, "five", 4))
                 return 5;
-            if (buff_ends_with(ctx, "six", 3))
+            if (b_ends_with(buff, "six", 3))
                 return 6;
-            if (buff_ends_with(ctx, "seven", 5))
+            if (b_ends_with(buff, "seven", 5))
                 return 7;
-            if (buff_ends_with(ctx, "eight", 5))
+            if (b_ends_with(buff, "eight", 5))
                 return 8;
-            if (buff_ends_with(ctx, "nine", 4))
+            if (b_ends_with(buff, "nine", 4))
                 return 9;
         } else if (ch == '\n') {
-            ctx->b_idx = 0;
+            b_clear(buff);
             return NEWLINE;
         } else if (ch >= '0' && ch <= '9') {
-            ctx->b_idx = 0;
+            b_clear(buff);
             return ch - '0';
         } else {
-            ctx->b_idx = 0; // unreachable but lets reset just in case
+            b_clear(buff); // unreachable but lets reset just in case
         }
     }
     return DONE;
@@ -95,12 +106,12 @@ int solve(bool p2) {
     int last_num = -1;
     int sum = 0;
 
-    Ctx ctx = {
-        .f = f,
-        .buff = malloc(0 * sizeof(int)),
-        .buff_size = 0,
-        .b_idx = 0,
-    };
+    Ctx ctx = {.f = f,
+               .buff = {
+                   .buff = NULL,
+                   .buff_size = 0,
+                   .b_idx = 0,
+               }};
 
     while ((n = next_int(&ctx, p2)) != DONE) {
         if (n == NEWLINE) {
